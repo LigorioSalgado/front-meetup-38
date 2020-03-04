38e534ef51643b413c47dcece7bd23646e9ea926
ligorio93 <edwin@devf.mx>
2020-03-03 19:58:30 -0600

    Card complete

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,23 @@
-import React from 'react';
+import React from "react";
 
-function Card(){
-    return(
-      <>
-            <div className="post-preview">
-            <a href="post.html">
-                <h2 className="post-title">
-                Man must explore, and this is exploration at its greatest
-                </h2>
-                <h3 className="post-subtitle">
-                Problems look mighty small from 150 miles up
-                </h3>
-            </a>
-            <p className="post-meta">Posted by
-                <a href="#">Start Bootstrap</a>
-                on September 24, 2019</p>
-            </div>
-            <hr/>
-       </> 
-    )
+function Card(props) {
+  return (
+    <>
+      <div className="post-preview">
+        <a href="post.html">
+          <h2 className="post-title">
+           {props.title}
+          </h2>
+        </a>
+        <p className="post-meta">
+          Posted by
+          <a href="#">{props.author}</a>
+          on {props.date}
+        </p>
+      </div>
+      <hr />
+    </>
+  );
 }
+
+export default Card;
